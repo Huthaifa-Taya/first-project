c725478da66d816d5932a28f910f78217e99b15f
taya huthaifa <thuthaifa@everconnect.com>
2022-11-02 16:08:01 +0300

    implemented the guessing using do while

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -22,7 +22,7 @@ def guess_with_while(number_to_guess, guess_number, range)
   end
   puts "Congrats you guessed the correct number!"
 end
-
+``
 def guess_with_do_while(number_to_guess, guess_number, range)
   while guess_number != number_to_guess do
     puts "Enter your guessing number between #{range[0]} and #{range[1]}"
@@ -33,12 +33,26 @@ def guess_with_do_while(number_to_guess, guess_number, range)
   puts "Congrats you guessed the correct number!"
 end
 
+def guess_with_loop_do(number_to_guess, guess_number, range)
+  loop do
+    puts "Enter your guessing number between #{range[0]} and #{range[1]}"
+    guess_number = gets.chomp.to_i
+    break if guess_number == number_to_guess
+    puts guess_number > number_to_guess ? "Your number is High" : "Your number is Low"
+  end
+  puts "Congrats you guessed the correct number!"
 
-puts "guessing using until"
-guess_with_until(number_to_guess, guessed_number, guessing_range)
+end
 
-puts "guessing using while"
-guess_with_while(number_to_guess, guessed_number, guessing_range)
 
-puts "guessing using do while"
-guess_with_do_while(number_to_guess, guessed_number, guessing_range)
+# puts "guessing using until"
+# guess_with_until(number_to_guess, guessed_number, guessing_range)
+#
+# puts "guessing using while"
+# guess_with_while(number_to_guess, guessed_number, guessing_range)
+#
+# puts "guessing using do while"
+# guess_with_do_while(number_to_guess, guessed_number, guessing_range)
+
+puts "guessing using loop do"
+guess_with_loop_do(number_to_guess, guessed_number, guessing_range)
