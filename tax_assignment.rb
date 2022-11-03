389ab86d11500ab51c8a13ff30f9a352cc40b442
taya huthaifa <thuthaifa@everconnect.com>
2022-11-03 12:08:16 +0300

    added method for salary input and validation

--- tax_assignment.rb
+++ tax_assignment.rb
@@ -3,3 +3,12 @@ def login (user_name, password)
   passing_password = "Admin123"
   password == passing_password and user_name == passing_user_name
 end
+
+def enter_validate_salary
+  wasta_message = "You are a Wasta man"
+  print "Kindly enter your monthly salary:  "
+  monthly_salary = gets.chomp.to_f
+  raise RangeError,"Invalid Salary" unless monthly_salary > 0
+  raise RangeError,wasta_message if monthly_salary > 5000.0
+  monthly_salary
+end
